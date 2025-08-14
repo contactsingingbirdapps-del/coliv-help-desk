bbf78ed648bf7ef7b9ddcfd6fc46b29937867a65
Ayush <ayushsinghzzzzz38@gmail.com>
2025-08-14 12:34:11 +0530

    v1.1

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.example.app',
-  appName: 'cohub management',
+  appName: 'cohub',
   webDir: 'dist'
 };
 
