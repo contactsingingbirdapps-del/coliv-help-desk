627a4f04f7145131b1637a6207e2a2e7a8fcd391
Ayush <ayushsinghzzzzz38@gmail.com>
2025-09-24 13:58:27 +0530

    app v1 for testing

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'CoLiv.help.desk',
+  appId: 'com.coliv.helpdesk',
   appName: 'cohub-help-desk-app',
   webDir: 'dist'
 };
