e1a3e0a25d7d398a0697a6bee64b6ec7bd6ff1cc
Ayush <ayushsinghzzzzz38@gmail.com>
2025-09-25 21:56:56 +0530

    package name issue resolved

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.coliv.helpdesk',
-  appName: 'cohub-help-desk-app',
+  appName: 'Coliv-Help Desk',
   webDir: 'dist'
 };
 
